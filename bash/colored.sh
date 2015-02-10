9f7b47454372096807dc0aef708d7255a8e9f66b
Rohit Chormale <rohitchormale@gmail.com>
2015-02-10 16:58:48 +0530

    Added bash colored script

---
+++ bash/colored.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#colors
+color_lblue='\033[1;34m'
+color_red='\033[1;31m'
+color_lgreen='\033[1;32m'
+color_custom='\033[1;33;44m'
+color_no='\033[0m'
+
+#ascii banner
+printf "${color_custom}" 
+cat <<EOF
+ _____ ___   ___  
+ |  ___/ _ \ / _ \ 
+ | |_ | | | | | | |
+ |  _|| |_| | |_| |
+ |_|   \___/ \___/ 
+
+EOF
+printf "${color_no}" 
+
+
