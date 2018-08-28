36a975d2909a36833b124950cba417d199448c5c
Rohit Chormale <rohitchormale@gmail.com>
2018-08-28 10:33:58 +0530

    added colored ps1

---
+++ bash/colored-ps1.sh
@@ -0,0 +1,6 @@
+# yo... colored ps1 for production servers... if RED do NOT touch when you drunk
+# add in ur bashrc
+# replace 'foobarbazqux' with whatever u want local-ip/name etc
+
+PS1="\[\e[1;31m\][\u@foobarbazqux \W]\\$\[\e[0m\] "
+
