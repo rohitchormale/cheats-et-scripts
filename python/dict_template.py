a77be57585faa1cac40e6b1d187920b709f81825
Rohit Chormale <rohitchormale@gmail.com>
2020-01-05 02:40:52 +0530

    Updated regex pattern. Updated test cases and docstrings accordingly

--- python/dict_template.py
+++ python/dict_template.py
@@ -7,25 +7,25 @@ This template can be filled by looking up values in another dictionary, similar 
 e.g.
 
 template = {
-  "key1": "(key1)s",
-  "key2": "(key2)s",
+  "key1": "%(key1)s",
+  "key2": "%(key2)s",
   "key3": [
     "foo",
     "bar",
     {
-      "key4": "(key1)s",
-      "key5": "(key1)s"
+      "key4": "%(key1)s",
+      "key5": "%(key1)s"
     }
   ],
   "key6": {
-    "key7": "(key7)s",
-    "key8": "(key8)s",
-    "key9": [
+    "key7": "%(key7)s",
+    "key8": "%(key8)s",
+    "%(key9)s": [
       "foo",
       {
-        "key10": "(key10)s",
-        "key11": "(key11)s",
-        "key12": "(key12)s"
+        "key10": "%(key10)s",
+        "key11": "%(key11)s",
+        "key12": "%(key12)s"
       },
       "bar"
     ]
@@ -81,14 +81,14 @@ output = {
 import re
 
 
-def fill_dict_template(template, values, key_lookup=True, lookup_regex="\((.*?)\)s"):
+def fill_dict_template(template, values, key_lookup=True, lookup_regex="%\((.*?)\)s"):
     """Fill complex dictionary templates with values from another dict.
   
     Parameters:
-    template (dict): Dictinory template with placeholders to fill up
+    template (dict): Dictionary template with placeholders to fill up
     values (dict): Dictionary of placeholder-values. Each placeholder in template will be replaced by related value in this dictionary 
-    key_lookup (boolean): By default, both key and values having placeholders will be filled up. To disable, key fill up, set this option False
-    lookup_regex (string): Provide custom regex for placeholder. Default placeholder will be catched in patter %(placeholder)s
+    key_lookup (boolean): By default, both key and values having placeholders will be filled up. To disable, key fill up, set this option 'False'
+    lookup_regex (string): Provide custom regex for placeholder. Default placeholder pattern will be '%(placeholder)s'
 
     Returns:
     dict: Template by filling up values
@@ -128,11 +128,11 @@ import unittest
 class FillDictTemplate(unittest.TestCase):
     def test_fill_dict_template(self):
         template = {
-            "key1": "(key1)s",
-            "key2": "(key2)s",
-            "key3": ["foo", "bar", {"key4": "(key1)s", "key5": "(key1)s"}],
-            "key6": {"key7": "(key7)s", "key8": "(key8)s", "key9": ["foo", {"key10": "(key10)s","key11": "(key11)s", "key12": "(key12)s"}, "bar"]},
-            "key13": "(key13missing)s"
+            "key1": "%(key1)s",
+            "key2": "%(key2)s",
+            "key3": ["foo", "bar", {"key4": "%(key1)s", "key5": "%(key1)s"}],
+            "key6": {"key7": "%(key7)s", "key8": "%(key8)s", "%(key9)s": ["foo", {"key10": "%(key10)s", "key11": "%(key11)s", "key12": "%(key12)s"}, "bar"]},
+            "key13": "%(key13missing)s"
         }
 
         values = {
@@ -144,7 +144,7 @@ class FillDictTemplate(unittest.TestCase):
             "key2": "key2",
             "key3": ["foo", "bar", {"key4": "key1", "key5": "key1"}],
             "key6": {"key7": "key7", "key8": "key8", "key9": ["foo", {"key10": "key10","key11": "key11", "key12": "key12"}, "bar"]},
-            "key13": "(key13missing)s"
+            "key13": "%(key13missing)s"
         }
         self.assertDictEqual(fill_dict_template(template, values), output)
 
