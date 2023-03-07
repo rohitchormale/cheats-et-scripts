d63286281eceb83f17c5bc1f43b9b38efaaf3cae
Rohit Chormale <rohitchormale@gmail.com>
2023-03-07 18:09:27 +0530

    added factorial example by recursive closure

--- go/all.go
+++ go/all.go
@@ -16,6 +16,7 @@ func learnRecursion() {
 
 
 	fmt.Println("--- recurvsive closure ---")
+	fmt.Println("fibonancy series using recursive closure")
 	var fib func(n int) int
 	fib = func(n int) int {
 		if n < 2 {
@@ -24,6 +25,16 @@ func learnRecursion() {
 		return fib(n-1) + fib(n-2)
 	}
 	fmt.Println(fib(7))
+
+	fmt.Println("factorial using recursive closure")
+	var fact func(n int) int 
+	fact = func(n int) int {
+		if n == 0 {
+			return 1
+		}
+		return n * fact(n-1) 
+	}
+	fmt.Println(fact(5))
 }
 
 
