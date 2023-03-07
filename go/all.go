e8dda552ae4edeb7a2878b009d85be48762969af
Rohit Chormale <rohitchormale@gmail.com>
2023-03-07 18:07:10 +0530

    added recursion examples

--- go/all.go
+++ go/all.go
@@ -3,19 +3,35 @@ package main
 import "fmt"
 
 
-func nextInt() func() int {
-	i := 0
+func main() {
+	learnClosures()
+	learnRecursion()
 
-	return func() int {
-		i++
-		return i
-	}
 }
 
 
-func main() {
-	learnClosures()
+func learnRecursion() {
+	fmt.Println("--- recursion ---")
+	fmt.Println(factorial(5))
 
+
+	fmt.Println("--- recurvsive closure ---")
+	var fib func(n int) int
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+	fmt.Println(fib(7))
+}
+
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
 }
 
 
@@ -30,3 +46,14 @@ func learnClosures() {
 	fmt.Println(next_number_2()) //output: 1
 
 }
+
+
+func nextInt() func() int {
+	i := 0
+
+	return func() int {
+		i++
+		return i
+	}
+}
+
